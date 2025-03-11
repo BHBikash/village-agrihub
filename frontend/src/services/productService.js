9234b73a950eb81cd2c250b118169e2d1789f5ca
MD Bokhtiar Hossain Bikash <110851635+BHBikash@users.noreply.github.com>
2025-03-11 17:06:29 +0600

    Update productService.js

--- frontend/src/services/productService.js
+++ frontend/src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/products";
+const API_URL = "http://https://village-agrihub-backend.onrender.com/api/products";
 
 export const getAllProducts = async () => {
   try {
